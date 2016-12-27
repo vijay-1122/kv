fd8a5394ae10eb08f8733037b74c350c1532f14b
vijay-1122 <vijay-1122@users.noreply.github.com>
2016-12-27 10:05:16 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+   int n;
+   printf("enter the number");
+   scanf("%d",&n);
+   if(n>0)
+   {
+       printf("it is positive");
+   }
+   if(n<0)
+   {
+       printf("it is negative");
+   }
+   else
+    {
+    printf("it is zero");
+}
+return 0;
+}
